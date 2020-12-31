f9bbc6b265cc3db7f3c3a46b3ba6301b01771f64
initializrc <panji.cahyountoro@gmail.com>
2021-01-01 04:08:49 +0700

    printing dan alur eksekusi java

--- java dasar/06 - Printing Dan Alur Eksekusi Java/src/com/tutorial/Main.java
+++ java dasar/06 - Printing Dan Alur Eksekusi Java/src/com/tutorial/Main.java
@@ -2,6 +2,11 @@ package com.tutorial;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        System.out.println("hello teman - teman");
+        System.out.println("baris ke-2 : hai juga akang pukis");
+
+        System.out.print("baris ke-3 : ini adalah baris ketiga");
+        System.out.print("baris ke-4 : saya meneruskan baris ketiga");
     }
 }
