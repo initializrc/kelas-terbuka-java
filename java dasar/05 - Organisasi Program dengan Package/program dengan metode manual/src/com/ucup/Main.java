c9df381648d5172b3fb8a20706952102e97e5e34
initializrc <panji.cahyountoro@gmail.com>
2021-01-01 03:43:08 +0700

    organisasi program dengan package

---
+++ java dasar/05 - Organisasi Program dengan Package/program dengan metode manual/src/com/ucup/Main.java
@@ -0,0 +1,21 @@
+//* public class Main{
+
+//*	public static void main(String[] args){
+//*		System.out.println("hello world");
+//*	}
+//*}
+
+/*public class Main{
+	public static void main(String[] args){
+		System.out.println("Hello world");
+	}
+}*/
+
+package com.ucup;
+
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Hi, this my first learn program java language");
+	}
+}
+
---
+++ java dasar/05 - Organisasi Program dengan Package/program dengan metode manual/src/programku/OtongSurotong.java
@@ -0,0 +1,11 @@
+package programku;
+
+class OtongSurotong{
+
+	public static void main(String[] args){
+
+		System.out.println("Namaku adalah otong, teman si ucup");
+		
+	}
+
+}
