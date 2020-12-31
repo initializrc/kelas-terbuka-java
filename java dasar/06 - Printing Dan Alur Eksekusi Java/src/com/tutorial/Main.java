2b98e88f1da5e913e197b259b92ac55a3136662d
initializrc <panji.cahyountoro@gmail.com>
2021-01-01 04:17:08 +0700

    printing dan alur eksekusi java

--- java dasar/06 - Printing Dan Alur Eksekusi Java/src/com/tutorial/Main.java
+++ java dasar/06 - Printing Dan Alur Eksekusi Java/src/com/tutorial/Main.java
@@ -8,5 +8,14 @@ public class Main {
 
         System.out.print("baris ke-3 : ini adalah baris ketiga");
         System.out.print("baris ke-4 : saya meneruskan baris ketiga");
+
+        System.out.print("baris ke-5 : hallo mahmuy???");
+        System.out.println("baris ke-6 : hallo juga hobloh!!!");
+
+        System.out.print("baris ke-7 : hai bro, mari kita kongkow \n");
+        System.out.print("baris ke-8 : kongkow kemana bro? \n");
+        System.out.print("baris ke-9 : mari kita kongkow ke citamiang!!!\n");
+
+        System.out.printf("wiro sableng naga geni %d", 212);
     }
 }
